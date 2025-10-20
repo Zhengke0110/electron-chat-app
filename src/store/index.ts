34a914d0dc20978e0e40fb0ec6193fc0e79d5901
zhengke <zhengke996@163.com>
2025-10-20 16:13:52 +0800

    feat: 删除 app 和 counter store，简化状态管理

--- src/store/index.ts
+++ src/store/index.ts
@@ -5,6 +5,4 @@ const pinia = createPinia();
 export default pinia;
 
 // 导出所有 stores
-export { useAppStore } from './app';
-export { useCounterStore } from './counter';
 export { useDbStore } from './db';
